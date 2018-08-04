34be592eae454760cab912541002f847899dd681
Yijing Zhang <yijingzhang2021@u.northwestern.edu>
2018-08-04 17:44:41 -0500

    option page structure

--- src/js/myapp.js
+++ src/js/myapp.js
@@ -1,70 +1,75 @@
-
 var sent_info_dict = new Object;
 var sent_info_dict = {};
 var receive_info_dict = new Object;
-var receive_info_dict= {};
+var receive_info_dict = {};
 
-InboxSDK.load(2, 'sdk_Catworks_b73c68555a').then(function(sdk){
 
-  sdk.Compose.registerComposeViewHandler(function(composeView){
+function Compose_view_info() {
+  InboxSDK.load(2, 'sdk_Catworks_b73c68555a').then(function (sdk) {
 
-    composeView.addButton({
-      title: "Get my email content!",
-      iconUrl: "https://encrypted-tbn1.gstatic.com/images?q=tbn:ANd9GcQxFoh469eOsZQkuPOLpZn3R6yyIExkZCxOxf4ywfeY3v330EwP3Q",
-      onClick: function(event) {
-        sent_info_dict.contact = event.composeView.getToRecipients();
-        sent_info_dict.subject = event.composeView.getSubject()
-        sent_info_dict.htmlcontent = event.composeView.getHTMLContent();
-        sent_info_dict.textcontent = event.composeView.getTextContent()
+      sdk.Compose.registerComposeViewHandler(function (composeView) {
 
+        composeView.addButton({
+          title: "Get my email content!",
+          iconUrl: "https://encrypted-tbn1.gstatic.com/images?q=tbn:ANd9GcQxFoh469eOsZQkuPOLpZn3R6yyIExkZCxOxf4ywfeY3v330EwP3Q",
+          onClick: function (event) {
+            sent_info_dict.contact = event.composeView.getToRecipients();
+            sent_info_dict.subject = event.composeView.getSubject()
+            sent_info_dict.htmlcontent = event.composeView.getHTMLContent();
+            sent_info_dict.textcontent = event.composeView.getTextContent()
+
+            console.log('Email Destination Name:')
+            console.log(sent_info_dict.contact[0].name);
+            console.log('Email Destination:')
+            console.log(sent_info_dict.contact[0].emailAddress);
+            console.log('Email Subject:')
+            console.log(sent_info_dict.subject);
+            console.log('Email Body - HTML')
+            console.log(sent_info_dict.htmlcontent);
+            console.log('Email Body - TEXT')
+            console.log(sent_info_dict.textcontent);
+          },
+        });
+      });
+    })
+  }
+
+  function Send_info() {
+    sdk.Compose.registerComposeViewHandler(function (composeView) {
+
+      composeView.on('sent', function (event) {
+        // message sent successfully
+        console.log("Message sent successfully.");
+        // print info
+        console.log(sent_info_dict.contact);
         console.log('Email Destination Name:')
         console.log(sent_info_dict.contact[0].name);
         console.log('Email Destination:')
         console.log(sent_info_dict.contact[0].emailAddress);
         console.log('Email Subject:')
         console.log(sent_info_dict.subject);
-        console.log('Email Body - HTML')
+        console.log('Email Body:')
         console.log(sent_info_dict.htmlcontent);
-        console.log('Email Body - TEXT')
         console.log(sent_info_dict.textcontent);
-      },
+      });
     });
-  });
+  }
 
-  sdk.Compose.registerComposeViewHandler(function(composeView) {
-
-    composeView.on('sent', function(event) {
-      // message sent successfully
-      console.log("Message sent successfully.");
-      // print info
-      console.log(sent_info_dict.contact);
-      console.log('Email Destination Name:')
-      console.log(sent_info_dict.contact[0].name);
-      console.log('Email Destination:')
-      console.log(sent_info_dict.contact[0].emailAddress);
-      console.log('Email Subject:')
-      console.log(sent_info_dict.subject);
-      console.log('Email Body:')
-      console.log(sent_info_dict.htmlcontent);
-      console.log(sent_info_dict.textcontent);
+  function conversation_info() {
+    sdk.Conversations.registerMessageViewHandler(function (messageView) {
+      messageView.addAttachmentIcon({
+        tooltip: "Load Email Data",
+        iconUrl: "https://encrypted-tbn1.gstatic.com/images?q=tbn:ANd9GcQxFoh469eOsZQkuPOLpZn3R6yyIExkZCxOxf4ywfeY3v330EwP3Q",
+        onClick: function (event) {
+          receive_info_dict.sender = messageView.getSender()
+          receive_info_dict.receivetime = messageView.getDateString()
+          console.log('Sender:')
+          console.log(receive_info_dict.sender)
+          console.log('Receiving Time:')
+          console.log(receive_info_dict.receivetime)
+        },
+        orderHint: 0,
+      });
     });
-  });
 
-  
-  sdk.Conversations.registerMessageViewHandler(function(messageView){
-    messageView.addAttachmentIcon({
-      tooltip: "Load Email Data",
-      iconUrl: "https://encrypted-tbn1.gstatic.com/images?q=tbn:ANd9GcQxFoh469eOsZQkuPOLpZn3R6yyIExkZCxOxf4ywfeY3v330EwP3Q",
-      onClick: function(event) {
-        receive_info_dict.sender = messageView.getSender()
-        receive_info_dict.receivetime = messageView.getDateString()
-        console.log('Sender:')
-        console.log(receive_info_dict.sender)
-        console.log('Receiving Time:')
-        console.log(receive_info_dict.receivetime)
-      },
-      orderHint: 0,
-    });
-  });
-
-});
+  }
