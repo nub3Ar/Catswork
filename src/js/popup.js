7e632be72b765473effa7c40187b5461be688c8b
Yijing Zhang <yijingzhang2021@u.northwestern.edu>
2018-09-09 15:06:56 -0500

    auto-saving

--- src/js/popup.js
+++ src/js/popup.js
@@ -4,5 +4,19 @@ jQuery(document).ready(function () {
 		selectMonths: true,
 		selectYears: 15
 	});
-});
 
+	var allInputs = $(":input");
+	allInputs.each(function (input) {
+		$(this).blur(function (input) {
+			localStorage.setItem($(this).attr('id'), $(this).val());
+			console.log("hello")
+		})
+		$(this).val(localStorage.getItem($(this).attr('id')))
+	})
+
+	$('#submit').click(function () {
+		localStorage.clear()
+	})
+
+
+});
