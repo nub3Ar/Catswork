03d60cb8eaf6c1d06d3c36a892146ebe6946324b
Yijing Zhang <yijingzhang2021@u.northwestern.edu>
2019-01-08 23:37:46 -0600

    testing version 2

---
+++ background.js
@@ -0,0 +1,10 @@
+function install_notice() {
+
+    var now = new Date().getTime();
+    localStorage.setItem('install_time', now);
+    chrome.tabs.create({url: "src/html/options.html"});
+
+    localStorage.setItem('first_time_user', true);
+    localStorage.setItem('tutorial_step', 1);
+}
+install_notice();
